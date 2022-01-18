64318274bbe51553f183c23d8315088e5e990497
Jen Caza <jencaza@gmail.com>
2022-01-17 20:28:34 -0500

    final changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import './App.css';
-import TextRepeaterButton from './components/TextRepeaterButton.js';
-import AngryButton from './components/AngryButton.js';
-import CounterButton from './components/CounterButton.js';
-import LightSwitchButton from './components/LightSwitchButton.js'
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+
+// Button Components
+import AngryButton from "./components/AngryButton";
+import CounterButton from "./components/CounterButton";
+import LightSwitchButton from "./components/LightSwitchButton";
+import TextRepeaterButton from "./components/TextRepeaterButton";
+
 
 function App() {
+  const [light, setLight] = useState("off");
+  const dark = light === "off" ? "dark" : "";
+  const switchLight = () => setLight(light === "on" ? "off" : "on");
+
   return (
-    <div className={`App`}>
+    <div className={`App ${dark}`}>
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton />
+        <LightSwitchButton light={light} switchLight={switchLight} />
         <TextRepeaterButton />
       </section>
     </div>
